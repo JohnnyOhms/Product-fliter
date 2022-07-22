84f3b5c2cddde0b36f041d37c8481320b2713a6f
JohnnyOhms <officialjohn662@gmail.com>
2022-07-22 13:00:12 +0400

    js added

---
+++ script.js
@@ -0,0 +1,24 @@
+const selectProduct = document.querySelectorAll('.select-product')
+const product = document.querySelectorAll(".items")
+
+selectProduct.forEach((btn)=>{
+    btn.addEventListener('click', (e)=>{
+        selectProduct.forEach((item)=>{
+            item.classList.remove("active")
+        })
+        btn.className += " active"
+
+        let fliter = btn.dataset.fliter;
+        
+        product.forEach((product)=>{
+            if (fliter == "all") {
+                product.style.display = "block"
+            }else if (product.classList.contains(fliter)) {
+                product.style.display = "block"
+            }else{
+                product.style.display = "none"
+            }
+        })
+
+    })
+})
